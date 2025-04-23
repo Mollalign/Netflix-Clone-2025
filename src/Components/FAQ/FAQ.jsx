b5a9a30c34c244d91201f594398ecff47c3050b9
Mollalign Daniel <molledan69@gmail.com>
2025-04-23 00:22:30 -0700

    edit FAQ

--- src/Components/FAQ/FAQ.jsx
+++ src/Components/FAQ/FAQ.jsx
@@ -14,26 +14,27 @@ const FAQ = () => {
     <div className="w-[92%] md:w-[80%] mx-auto text-white">
       <h2 className="text-2xl font-bold my-5 text-center">Frequently Asked Questions</h2>
       <div className="space-y-2.5">
-        {faqs.map((faq, index) => (
-          <div key={index} className="bg-neutral-800 rounded-md overflow-hidden border border-white/20">
-            <button
-              className="w-full flex justify-between items-center px-4 py-4.5 md:py-5 text-left focus:outline-none cursor-pointer"
-              onClick={() => toggle(index)}
-            >
-              <span className="text-lg font-medium">{faq.question}</span>
-              <ChevronDown
-                className={`transition-transform duration-300 ${
-                  openIndex === index ? "rotate-180" : ""
-                }`}
-              />
-            </button>
-            {openIndex === index && (
-              <div className="px-4 pb-4 text-sm text-gray-300">
-                {faq.answer}
-              </div>
-            )}
+      {faqs.map((faq, index) => (
+        <div key={index} className="bg-neutral-800 rounded-md overflow-hidden transition-all duration-300">
+          <button
+            className="w-full flex justify-between items-center px-4 py-4 text-left focus:outline-none"
+            onClick={() => toggle(index)}
+          >
+            <span className="text-lg font-medium">{faq.question}</span>
+            <ChevronDown
+              className={`transition-transform duration-300 ${openIndex === index ? "rotate-180" : ""}`}
+            />
+          </button>
+
+          <div
+            className={`px-4 overflow-hidden transition-all duration-300 ease-in-out 
+            ${openIndex === index ? "max-h-50 py-3" : "max-h-0 py-0"}`}
+          >
+            <p className="text-sm text-gray-300 font-semibold">{faq.answer}</p>
           </div>
-        ))}
+        </div>
+      ))}
+
       </div>
 
       <div className="mt-6 flex justify-center">
